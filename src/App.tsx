e771d07e6acaf7da45d9359c742ae1a44aa86711
Ami Ashman <amiashman8@gmail.com>
2026-03-24 17:06:38 -0400

    Added About Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import UnderConstruction from "./pages/UnderConstruction";
 import CV from "./pages/CV";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import { cvProps } from "./assets/cvInformation";
 import ProjectDispatch from "./pages/ProjectDispatch";
 import FourZeroFour from "./pages/FourZeroFour";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<UnderConstruction />} />
+        <Route path="/about" element={<About />} />
         <Route path="/cv" element={<CV {...cvProps} />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element={<ProjectDispatch />} />
